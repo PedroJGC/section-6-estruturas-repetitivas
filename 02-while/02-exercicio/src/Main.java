a310d2f89ef178e2f1fdb74e1a297af60792f59c
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-03 18:21:27 -0300

    exercise 02: while loop structure

---
+++ 02-while/02-exercicio/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Exercício 02
+        * Escreva um programa para ler as coordenadas (X,Y) de uma quantidade
+        *  indeterminada de pontos no sistema cartesiano. Para cada ponto
+        * escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+        * menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+        * */
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 && y != 0 ) {
+            if (x > 0 && y > 0) {
+                System.out.println("Primeiro");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Segundo");
+            } else if (x < 0 && y < 0) {
+                System.out.println("Terceiro");
+            } else if (x > 0 && y < 0) {
+                System.out.println("Quarto");
+            }
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
