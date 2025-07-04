400135d81866d39e697e724f91c73759d2ea02a0
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-04 18:36:30 -0300

    exercise 03: For loop structure

--- 02-while/02-exercicio/src/Main.java
+++ 02-while/02-exercicio/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         /*
         * Exercício 02
-        * Escreva um programa para ler as coordenadas (X,Y) de uma quantidade
+        * Escreva um programa para ler as coordenadas (X, Y) de uma quantidade
         *  indeterminada de pontos no sistema cartesiano. Para cada ponto
         * escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
         * menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
